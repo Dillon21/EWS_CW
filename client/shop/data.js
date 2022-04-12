9b505e9d26ceb09caa1da5092ee426ee41e13642
Dillon21 <dillon9821@gmail.com>
2022-04-12 14:29:16 +0100

    fix images

--- client/shop/data.js
+++ client/shop/data.js
@@ -1,13 +1,13 @@
-import img1 from './images/Cap.jpg';
-import img2 from './images/Cat Beret.jpg';
-import img3 from './images/Cat Fedora.jpg';
-import img4 from './images/Cat Scarf.jpg';
-import img5 from './images/Doggiedora.jpg';
-import img6 from './images/Jester Costume.jpg';
-import img7 from './images/Patriot pup.jpg';
-import img8 from './images/Pilot hat.jpg';
-import img9 from './images/Top hat.jpg';
-import img10 from './images/Ushanka.jpg';
+import img1 from './Images/Cap.jpg';
+import img2 from './Images/Cat Beret.jpg';
+import img3 from './Images/Cat Fedora.jpg';
+import img4 from './Images/Cat Scarf.jpg';
+import img5 from './Images/Doggiedora.jpg';
+import img6 from './Images/Jester Costume.jpg';
+import img7 from './Images/Patriot pup.jpg';
+import img8 from './Images/Pilot hat.jpg';
+import img9 from './Images/Top hat.jpg';
+import img10 from './Images/Ushanka.jpg';
 //fix me
 
 const data = {
